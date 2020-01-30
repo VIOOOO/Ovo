3d4e068a4b83b1975b527d432458318f38b6717c
enotsss <enochwesss@gmail.com>
2020-01-30 17:35:31 +0800

    备份 flutter listView 列表排列方式 网格布局以及下拉刷新，上拉新增

---
+++ flutter/flutterBak/flutter_ListView_GridView/lib/res/listData.dart
@@ -0,0 +1,44 @@
+List listData = [
+  {
+    "title": 'Candy Shop',
+    "author": 'Mohamed Chahin',
+    "imageUrl":
+        'https://ossweb-img.qq.com/upload/adw/image/20200120/68498e64124410e35d8c4f1e85a9a790.jpeg',
+  },
+  {
+    "title": 'Alibab',
+    "author": 'Alibab Shop',
+    "imageUrl":
+        'https://ossweb-img.qq.com/upload/adw/image/20200121/d229ba7ab0c06f88de7dc687a4e29a92.jpeg',
+  },
+  {
+    "title": 'Alibab',
+    "author": 'Alibab Shop',
+    "imageUrl":
+        'https://ossweb-img.qq.com/upload/adw/image/20200121/d229ba7ab0c06f88de7dc687a4e29a92.jpeg',
+  },
+  {
+    "title": 'Candy Shop',
+    "author": 'Mohamed Chahin',
+    "imageUrl":
+        'https://ossweb-img.qq.com/upload/adw/image/20200120/68498e64124410e35d8c4f1e85a9a790.jpeg',
+  },
+  {
+    "title": 'Alibab',
+    "author": 'Alibab Shop',
+    "imageUrl":
+        'https://ossweb-img.qq.com/upload/adw/image/20200121/d229ba7ab0c06f88de7dc687a4e29a92.jpeg',
+  },
+  {
+    "title": 'Candy Shop',
+    "author": 'Mohamed Chahin',
+    "imageUrl":
+        'https://ossweb-img.qq.com/upload/adw/image/20200120/68498e64124410e35d8c4f1e85a9a790.jpeg',
+  },
+  {
+    "title": 'Candy Shop',
+    "author": 'Mohamed Chahin',
+    "imageUrl":
+        'https://ossweb-img.qq.com/upload/adw/image/20200120/68498e64124410e35d8c4f1e85a9a790.jpeg',
+  },
+];
