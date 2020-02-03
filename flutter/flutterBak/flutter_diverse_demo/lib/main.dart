108a9a1343b151aff40f548dfc414e2a8cb42cc4
enotsss <enochwesss@gmail.com>
2020-02-03 20:10:53 +0800

    flutter 小组件例子

---
+++ flutter/flutterBak/flutter_diverse_demo/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter demo'),
+        ),
+        body: HomePage(),
+      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Demo'),
+    );
+  }
+}
+
+
+
