d93f9f5158c30416cc5df2e94075dbb8854c1f2b
enotsss <enochwesss@gmail.com>
2020-02-07 14:55:05 +0800

    flutter Drawer 抽屉组件 侧边栏、以及侧边栏内容布局

---
+++ flutter/flutterBak/flutter_diverse_demo/demo_lib/flutter_Drawer/lib/routes/Routes.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import '../pages/Tabs.dart';
+
+import '../pages/User.dart';
+
+
+// 配置命名路由
+final routes = {
+  // 根路由
+  '/': (context) => Tabs(),
+  '/user': (context) => UserPage(),
+};
+
+//  固定路由写法
+// 创建路由处理方法
+var onGenerateRoute = (RouteSettings settings) {
+  // print(settings.name);
+  final String name = settings.name;
+  // 获取名称对应的方法并赋值到 pageContentBuilder
+  final Function pageContentBuilder = routes[name];
+
+  if (pageContentBuilder != null) {
+    if (settings.arguments != null) {
+      // 命名路由内传参
+      final Route route = MaterialPageRoute(
+        builder: (context) =>
+            pageContentBuilder(context, arguments: settings.arguments),
+      );
+      return route;
+    } else {
+      // 无需传参
+      final Route route = MaterialPageRoute(
+        builder: (context) => pageContentBuilder(context),
+      );
+      return route;
+    }
+  }
+};
