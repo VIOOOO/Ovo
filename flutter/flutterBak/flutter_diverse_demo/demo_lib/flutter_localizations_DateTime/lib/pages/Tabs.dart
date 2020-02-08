c6e19fdb5ee169c89e488c2fe37caa6910cebe64
enotsss <enochwesss@gmail.com>
2020-02-08 20:13:18 +0800

    flutter localizations 汉化 DateTime 官方自带的日期组件和第三方日期组件

--- flutter/flutterBak/flutter_diverse_demo/demo_lib/flutter_localizations_DateTime/lib/pages/Tabs.dart
+++ flutter/flutterBak/flutter_diverse_demo/demo_lib/flutter_localizations_DateTime/lib/pages/Tabs.dart
@@ -33,7 +33,7 @@ class _TabsState extends State<Tabs> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter TextiField 单行多行文本框'),
+        title: Text('Flutter DateTime日期处理和汉化'),
       ),
       // 根据 tab 页签改变切换页面
       body: this._pageList[this._currentIndex],
