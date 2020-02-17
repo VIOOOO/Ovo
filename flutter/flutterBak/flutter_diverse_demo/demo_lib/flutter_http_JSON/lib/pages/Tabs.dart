7feaee72f455d299689a0b42df4b85ac9adc0695
enotsss <enochwesss@gmail.com>
2020-02-17 17:33:03 +0800

    flutter 网络请求 以及 JSON 字符串和 Map 类型转换

--- flutter/flutterBak/flutter_diverse_demo/demo_lib/flutter_http_JSON/lib/pages/Tabs.dart
+++ flutter/flutterBak/flutter_diverse_demo/demo_lib/flutter_http_JSON/lib/pages/Tabs.dart
@@ -33,7 +33,7 @@ class _TabsState extends State<Tabs> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter 弹出窗'),
+        title: Text('Flutter http 网络请求'),
       ),
       // 根据 tab 页签改变切换页面
       body: this._pageList[this._currentIndex],
