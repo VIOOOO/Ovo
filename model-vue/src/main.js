8c661ab9aed0ed32c89fec1c0e44ea0496fe5376
VIOOOO <enochwesss@gmail.com>
2020-12-01 17:02:38 +0800

    fullpage demo jq

--- model-vue/src/main.js
+++ model-vue/src/main.js
@@ -1,8 +1,10 @@
+import 'es6-promise/auto'
+// import myvue from './myvue'
+
 import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
+import App from './App'
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+// myvue();
